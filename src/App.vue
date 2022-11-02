854ad88d0ea6836173a777365fc72101192d9faf
KausaChangs <changayakausa@gmail.com>
2022-11-02 13:34:35 +0200

    need to add watch function to save name

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,12 @@ const todos_asc = computed(() =>
 );
 </script>
 
-<template></template>
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
+</template>
